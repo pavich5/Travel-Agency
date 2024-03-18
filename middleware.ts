25abdd21d50168b3539b5487ced5511347ab81f8
BogdanskoQR <pavic.antonio969@gmail.com>
2024-03-18 21:01:31 +0100

    fixed routes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  // Empty array for publicRoutes makes every route public
-  publicRoutes: [],
-  // Corrected syntax for ignoredRoutes
-  ignoredRoutes: ['/no-auth-in-this-route'],
+
+  publicRoutes: ["/",'/api/createlink','/api/getStripeLink','/cancelled','/vacation/list/:type','/vacation/:name', '/hotel/:name','/offer/:id', '/ai','/booking/confirmation/:id','/booking/confirmed',],
 });
  
-export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
