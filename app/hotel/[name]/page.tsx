1507a99f71c889172cc38c0842d17a497726d581
BogdanskoQR <pavic.antonio969@gmail.com>
2024-04-10 22:34:57 +0200

    added spinner when data loading

--- app/hotel/[name]/page.tsx
+++ app/hotel/[name]/page.tsx
@@ -1,34 +1,40 @@
-"use client"
-import { useEffect, useState } from 'react';
-import { Offer } from '@/app/types';
-import { vacationsCategories } from '@/app/Data/data';
-import styles from './page.module.css';
-import HotelDetails from '@/app/Components/HotelDetails/HotelDetails';
-import { findCountryName } from '@/app/utils/helper';
+"use client";
+import { useEffect, useState } from "react";
+import { Offer } from "@/app/types";
+import { vacationsCategories } from "@/app/Data/data";
+import styles from "./page.module.css";
+import HotelDetails from "@/app/Components/HotelDetails/HotelDetails";
+import { findCountryName } from "@/app/utils/helper";
+import { Spin } from "antd";
 const Page = ({ params }: any) => {
-    const [foundHotel, setFoundHotel] = useState<Offer | undefined>();
-    const decodedName = decodeURIComponent(params.name);
+  const [foundHotel, setFoundHotel] = useState<Offer | undefined>();
+  const decodedName = decodeURIComponent(params.name);
 
-    useEffect(() => {
-        const foundHotel = vacationsCategories.categories
-            .flatMap((category) => category.countrys)
-            .flatMap((country) => country.offers)
-            .find((offer) => offer.hotelName === decodedName);
-        // @ts-ignore
-        setFoundHotel(foundHotel);
-    }, [params, decodedName]);
+  useEffect(() => {
+    const foundHotel = vacationsCategories.categories
+      .flatMap((category) => category.countrys)
+      .flatMap((country) => country.offers)
+      .find((offer) => offer.hotelName === decodedName);
+    // @ts-ignore
+    setFoundHotel(foundHotel);
+  }, [params, decodedName]);
 
-    return (
-        <div className={styles.mainPageWrapper}>
-            {foundHotel ? (
-                <HotelDetails foundHotel={foundHotel} findCountryName={() => findCountryName(decodedName, vacationsCategories)} />
-            ) : (
-                <img src="https://dpauls.com/_nuxt/img/hotel-not-found.e006c8a.png" alt="" />
-            )}
+  return (
+    <div className={styles.mainPageWrapper}>
+      {foundHotel ? (
+        <HotelDetails
+          foundHotel={foundHotel}
+          findCountryName={() =>
+            findCountryName(decodedName, vacationsCategories)
+          }
+        />
+      ) : (
+        <div className={styles.skeletonWrapper}>
+          <Spin size="large" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
-
-
 
 export default Page;
