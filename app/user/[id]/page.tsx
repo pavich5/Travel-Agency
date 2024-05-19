ad9eea66d5be9c05a0b98486ce1583de84030630
BogdanskoQR <pavic.antonio969@gmail.com>
2024-05-19 22:52:52 +0200

    css

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,6 +1,8 @@
-"use client";
+"use client"
 import { useUser, useClerk } from "@clerk/nextjs";
 import styles from "./page.module.css";
+import React, { useState } from 'react';
+import { DownOutlined,RightOutlined } from '@ant-design/icons';
 
 interface Payment {
   amount: number;
@@ -24,34 +26,48 @@ const UserDetails: React.FC = () => {
   const { user } = useUser();
   const { signOut } = useClerk();
 
-  //@ts-ignore
+  const [showPayments, setShowPayments] = useState(true);
+
+  // @ts-ignore
   const payments: Payment[] = user?.unsafeMetadata?.allPayments || [];
+
+  const togglePayments = () => {
+    setShowPayments(!showPayments);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.userDetails}>
-        <img
-          src={user?.imageUrl}
-          alt={user?.fullName || "User Image"}
-          className={styles.userImage}
-        />
-        <div className={styles.userInfo}>
-          <h1>{user?.fullName}</h1>
-          <p>
-            <strong>Email:</strong> {user?.primaryEmailAddress?.emailAddress}
-          </p>
-          <p>
-            <strong>Username:</strong> {user?.fullName}
-          </p>
-          <button onClick={() => signOut()} className={styles.signOutButton}>
-            Sign Out
-          </button>
+        <div className={styles.profile}>
+          <img
+            src={user?.imageUrl}
+            alt={user?.fullName || "User Image"}
+            className={styles.userImage}
+          />
+          <div className={styles.userInfo}>
+            <h1>{user?.fullName}</h1>
+            <p>
+              <strong>Email:</strong> {user?.primaryEmailAddress?.emailAddress}
+            </p>
+            <p>
+              <strong>Username:</strong> {user?.fullName}
+            </p>
+            <button onClick={() => signOut()} className={styles.signOutButton}>
+              Sign Out
+            </button>
+          </div>
         </div>
       </div>
 
       <div className={styles.paymentsSection}>
-        <h2>Payment History</h2>
-        {payments.length > 0 ? (
+        <h2 onClick={togglePayments} className={styles.paymentsHeader}>
+          Payment History ({payments.length}) {showPayments ? (
+            <DownOutlined />
+          ): (
+            <RightOutlined />
+          )}
+        </h2>
+        {showPayments && payments.length > 0 && (
           <div className={styles.paymentsList}>
             {payments.map((payment, index) => (
               <div key={index} className={styles.paymentItem}>
@@ -60,49 +76,52 @@ const UserDetails: React.FC = () => {
                   alt="Offer Image"
                   className={styles.offerImage}
                 />
-                <p>
-                  <strong>Amount:</strong> {payment.amount / 100}{" "}
-                  {payment.currency.toUpperCase()}
-                </p>
-                <p>
-                  <strong>Date:</strong>{" "}
-                  {new Date(payment.created * 1000).toLocaleString()}
-                </p>
-                <p>
-                  <strong>Payment ID:</strong> {payment.paymentIntentId}
-                </p>
-                <p>
-                  <strong>Payment Method Types:</strong>{" "}
-                  {payment.payment_method_types?.join(", ")}
-                </p>
-                <p>
-                  <strong>Hotel Name:</strong> {payment.hotelName}
-                </p>
-                <p>
-                  <strong>Country Name:</strong> {payment.countryName}
-                </p>
-                <p>
-                  <strong>Start Date:</strong> {payment.startDate}
-                </p>
-                <p>
-                  <strong>Location:</strong> {payment.location}
-                </p>
-                <p>
-                  <strong>End Date:</strong> {payment.endDate}
-                </p>
-                <p>
-                  <strong>Hotel City:</strong> {payment.hotelCity}
-                </p>
-                <p>
-                  <strong>Duration:</strong> {payment.duration}
-                </p>
-                <p>
-                  <strong>Quantity:</strong> {payment.qty}
-                </p>
+                <div className={styles.paymentDetails}>
+                  <p>
+                    <strong>Amount:</strong> {payment.amount / 100}{" "}
+                    {payment.currency.toUpperCase()}
+                  </p>
+                  <p>
+                    <strong>Date:</strong>{" "}
+                    {new Date(payment.created * 1000).toLocaleString()}
+                  </p>
+                  <p>
+                    <strong>Payment ID:</strong> {payment.paymentIntentId}
+                  </p>
+                  <p>
+                    <strong>Payment Method Types:</strong>{" "}
+                    {payment.payment_method_types?.join(", ")}
+                  </p>
+                  <p>
+                    <strong>Hotel Name:</strong> {payment.hotelName}
+                  </p>
+                  <p>
+                    <strong>Country Name:</strong> {payment.countryName}
+                  </p>
+                  <p>
+                    <strong>Start Date:</strong> {payment.startDate}
+                  </p>
+                  <p>
+                    <strong>Location:</strong> {payment.location}
+                  </p>
+                  <p>
+                    <strong>End Date:</strong> {payment.endDate}
+                  </p>
+                  <p>
+                    <strong>Hotel City:</strong> {payment.hotelCity}
+                  </p>
+                  <p>
+                    <strong>Duration:</strong> {payment.duration}
+                  </p>
+                  <p>
+                    <strong>Quantity:</strong> {payment.qty}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
-        ) : (
+        )}
+        {showPayments && payments.length === 0 && (
           <p>No payments found.</p>
         )}
       </div>
