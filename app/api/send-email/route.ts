481d14f719ed6c96a64adbf72026940fc02549b2
BogdanskoQR <pavic.antonio969@gmail.com>
2024-07-19 01:31:31 +0200

    fix email

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,17 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
+import nextCors from "nextjs-cors";
 import { sendEmail } from "../sendEmail";
 
-export async function POST(req: NextRequest) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Handle CORS
+  await nextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  });
+
   if (req.method === "POST") {
     try {
-        const { toEmail, subject, text, userName,offerId } = await req.json();
-        const result = await sendEmail(toEmail, subject, text,userName,offerId);
-        return new NextResponse(JSON.stringify(result), { status: 200 });
+      const { toEmail, subject, text, userName, offerId } = req.body;
+      const result = await sendEmail(toEmail, subject, text, userName, offerId);
+      res.status(200).json(result);
     } catch (error) {
       console.error("Error handling webhook:", error);
-      return new NextResponse();
-    }   
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   } else {
-    return new NextResponse();
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 }
