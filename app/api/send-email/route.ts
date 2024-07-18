d4ad91631991a149bd6bd18705a911b917bb9aae
BogdanskoQR <pavic.antonio969@gmail.com>
2024-07-19 01:33:31 +0200

    Update route.ts

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,26 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import nextCors from "nextjs-cors";
+import { NextRequest, NextResponse } from "next/server";
 import { sendEmail } from "../sendEmail";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Handle CORS
-  await nextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200,
-  });
+export async function POST(req: NextRequest) {
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, {
+      status: 204,
+      headers: corsHeaders,
+    });
+  }
 
   if (req.method === "POST") {
     try {
-      const { toEmail, subject, text, userName, offerId } = req.body;
+      const { toEmail, subject, text, userName, offerId } = await req.json();
       const result = await sendEmail(toEmail, subject, text, userName, offerId);
-      res.status(200).json(result);
+
+      return new NextResponse(JSON.stringify(result), {
+        status: 200,
+        headers: corsHeaders,
+      });
     } catch (error) {
-      console.error("Error handling webhook:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+      console.error("Error handling request:", error);
+
+      return new NextResponse(null, {
+        status: 500,
+        headers: corsHeaders,
+      });
     }
   } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+    return new NextResponse(null, {
+      status: 405,
+      headers: corsHeaders,
+    });
   }
 }
