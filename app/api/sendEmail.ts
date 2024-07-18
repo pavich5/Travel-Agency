e4a50ddb0bbaa799692ced1ce00007572a6595e6
BogdanskoQR <pavic.antonio969@gmail.com>
2024-07-18 17:48:26 +0200

    fixed package issues

--- app/api/sendEmail.ts
+++ app/api/sendEmail.ts
@@ -1,4 +1,13 @@
-const mailjet = require('node-mailjet').connect(process.env.MAILJET_API, process.env.MAILJET_SECRET_KEY);
+const Mailjet = require('node-mailjet');
+
+const mailjet = Mailjet.apiConnect(
+  process.env.MAILJET_API,
+  process.env.MAILJET_SECRET_KEY,
+  {
+    config: {},
+    options: {}
+  }
+);
 
 export const sendEmail = async (toEmail:string, subject:string, text:string) => {
   try {
@@ -8,22 +17,24 @@ export const sendEmail = async (toEmail:string, subject:string, text:string) => 
         Messages: [
           {
             From: {
-              Email: 'pavic.antonio969@gmail.com',
+              Email: 'pavic.antonio969@gmail.com', // Your email address
               Name: 'Antonio Pavic',
             },
             To: [
               {
                 Email: toEmail,
-                Name: 'Recipient Name',
+                Name: 'Recipient Name', // Adjust if you have the recipient's name
               },
             ],
             Subject: subject,
             TextPart: text,
+            HTMLPart: `<h3>${text}</h3>`, // Use text for the HTML part too or customize
           },
         ],
       });
 
     const result = await request;
+    console.log(result.body);
     return result.body;
   } catch (error) {
     console.error("Error sending email:", error);
