87112f997dc994f40b03e876613cc192f90437dc
BogdanskoQR <pavic.antonio969@gmail.com>
2024-12-01 02:22:10 +0100

    fixing pipeline

--- app/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-// import { dbConnect } from "@/app/api/lib/db";
-export async function GET() {
-//   const con = await dbConnect();
-//   console.log("hit db connect",con);
-  return new NextResponse("connected and disconnected");
-}
