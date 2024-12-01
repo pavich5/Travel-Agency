763cb5cab83c840ef84b6756fde8bcef8367a623
BogdanskoQR <pavic.antonio969@gmail.com>
2024-12-01 02:48:35 +0100

    fix route

--- app/api/checkdb/route.ts
+++ app/api/checkdb/route.ts
@@ -1,5 +1,11 @@
-import { NextResponse } from 'next/server';
+import { dbConnect } from "@/app/api/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function GET(req:NextRequest) {
+  try {
+    await dbConnect();
     return new NextResponse("test");
+  } catch (error) {
+    return new NextResponse("Error", { status: 500 });
+  }
 }
