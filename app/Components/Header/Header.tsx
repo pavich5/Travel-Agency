9329d1ba6f8ce3d8c266d8d14f0ca1cdabf6ace0
BogdanskoQR <pavic.antonio969@gmail.com>
2024-12-01 02:30:05 +0100

    typo

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,30 +1,29 @@
-"use client"
-import styles from './Header.module.css';
-import Link from 'next/link';
-import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/nextjs';
-import { Dropdown, Space, Menu } from 'antd';
-import { LeftOutlined, DownOutlined } from '@ant-design/icons';
-import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
-import { useRouter, usePathname } from 'next/navigation';
-import { CountryLists } from '@/app/mocks/data';
+"use client";
+import styles from "./Header.module.css";
+import Link from "next/link";
+import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
+import { Dropdown, Space, Menu } from "antd";
+import { LeftOutlined, DownOutlined } from "@ant-design/icons";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import { useRouter, usePathname } from "next/navigation";
+import { CountryLists } from "@/app/mocks/data";
 const Header = () => {
   const router = useRouter();
-  const pathname = usePathname()
+  const pathname = usePathname();
   const { user } = useUser();
-
 
   const handleBack = () => {
     router.back();
   };
 
   const items = [
-    { key: '1', label: 'Summer Trips', href: '/vacation/list/Summer' },
-    { key: '2', label: 'Winter Trips', href: '/vacation/list/Winter' },
-    { key: '3', label: 'Easter Trips', href: '/vacation/list/Easter' },
-    { key: '4', label: 'Spring Trips', href: '/vacation/list/Spring' },
+    { key: "1", label: "Summer Trips", href: "/vacation/list/Summer" },
+    { key: "2", label: "Winter Trips", href: "/vacation/list/Winter" },
+    { key: "3", label: "Easter Trips", href: "/vacation/list/Easter" },
+    { key: "4", label: "Spring Trips", href: "/vacation/list/Spring" },
   ];
 
-  const menuItems = items.map(item => (
+  const menuItems = items.map((item) => (
     <Menu.Item key={item.key}>
       <Link href={item.href}>{item.label}</Link>
     </Menu.Item>
@@ -33,44 +32,62 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        {pathname !== '/' && <LeftOutlined onClick={handleBack} />}
-        <img src="https://images-platform.99static.com/zudNWGHtYiWa-sqd5jqXyVt6wBE=/0x0:1773x1773/500x500/top/smart/99designs-contests-attachments/133/133463/attachment_133463156" alt="Logo" />
-        <Link href='/' className={styles.logoText}>Globetrotter</Link>
+        {pathname !== "/" && <LeftOutlined onClick={handleBack} />}
+        <img
+          src="https://images-platform.99static.com/zudNWGHtYiWa-sqd5jqXyVt6wBE=/0x0:1773x1773/500x500/top/smart/99designs-contests-attachments/133/133463/attachment_133463156"
+          alt="Logo"
+        />
+        <Link href="/" className={styles.logoText}>
+          Globetrotter
+        </Link>
       </div>
       <HamburgerMenu />
       <div className={styles.navMenu}>
-      <div className={styles.menuItem}>
-          <Link href='/about' className={styles.menuItemText}>About us</Link>
+        <div className={styles.menuItem}>
+          <Link href="/about" className={styles.menuItemText}>
+            About us
+          </Link>
         </div>
         <div className={styles.menuItem}>
           <Dropdown overlay={<Menu>{menuItems}</Menu>}>
             <a onClick={(e) => e.preventDefault()}>
-              <Space>
-                Trips
-              </Space>
+              <Space>Trips</Space>
             </a>
           </Dropdown>
         </div>
         <div className={styles.menuItem}>
-          <Link href='/ai' className={styles.menuItemText}>Travel AI</Link>
+          <Link href="/ai" className={styles.menuItemText}>
+            Travel AI
+          </Link>
         </div>
-       
+
         <div className={styles.menuItem}>
-          <Link href='/blogs' className={styles.menuItemText}>Travel Experiences</Link>
+          <Link
+            style={{
+              cursor: "not-allowed",
+              textDecoration: "none",
+            }}
+            href="/"
+            className={styles.menuItemText}
+          >
+            Travel Experiences
+          </Link>
         </div>
         <div className={styles.menuItem}>
           <Dropdown
             overlay={
               <Menu>
-                {CountryLists.map((country) =>
-                  <Menu.Item key={country} style={{ padding: '8px', width: '200px' }}>
+                {CountryLists.map((country) => (
+                  <Menu.Item
+                    key={country}
+                    style={{ padding: "8px", width: "200px" }}
+                  >
                     <Link href={`/vacation/${country}`}>{country}</Link>
                   </Menu.Item>
-
-                )}
+                ))}
               </Menu>
             }
-            trigger={['click']}
+            trigger={["click"]}
           >
             <a onClick={(e) => e.stopPropagation()}>
               <Space>
@@ -78,25 +95,31 @@ const Header = () => {
                   Countries
                   <DownOutlined />
                 </div>
-
               </Space>
             </a>
           </Dropdown>
         </div>
-        <div className={styles.menuItem} style={{ marginLeft: '0px' }}>
+        <div className={styles.menuItem} style={{ marginLeft: "0px" }}>
           <SignedIn>
-            <div className={styles.userImage} onClick={()=> {router.push(`/user/${user?.id}`)}}>
+            <div
+              className={styles.userImage}
+              onClick={() => {
+                router.push(`/user/${user?.id}`);
+              }}
+            >
               <img src={user?.imageUrl} alt="" />
               <p>My Profile</p>
             </div>
           </SignedIn>
           <SignedOut>
-            <Link href='/sign-in' className={styles.menuItemText}>Login</Link>
+            <Link href="/sign-in" className={styles.menuItemText}>
+              Login
+            </Link>
           </SignedOut>
         </div>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
