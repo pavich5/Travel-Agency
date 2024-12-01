1e7c42171ca594b9b64f335732763b145f533161
BogdanskoQR <pavic.antonio969@gmail.com>
2024-12-01 02:11:03 +0100

    fix

--- app/api/checkdb/route.ts
+++ app/api/checkdb/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
-import { dbConnect } from "@/app/api/lib/db";
-export async function GET() {
-  const con = await dbConnect();
-  console.log("hit db connect",con);
-  return new NextResponse("connected and disconnected");
-}
+// import { NextResponse } from "next/server";
+// import { dbConnect } from "@/app/api/lib/db";
+// export async function GET() {
+//   const con = await dbConnect();
+//   console.log("hit db connect",con);
+//   return new NextResponse("connected and disconnected");
+// }
